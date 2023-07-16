c62ae9eb878bcf90920106ce1c5a5985e7da815e
Raoul G <raoulgbadou@gmail.com>
2023-07-16 15:12:32 -0700

    task 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() {
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void) {
     puts("Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
